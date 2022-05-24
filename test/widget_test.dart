a15c448a30770d6be051050477887283c7d9dd2b
BrunoSabadini <bruno_sabadini@hotmail.com>
2022-05-24 00:13:57 -0300

    forget this one

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:work_project/WalletPage/WalletBuild.dart';
 
-import 'package:work_project/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
