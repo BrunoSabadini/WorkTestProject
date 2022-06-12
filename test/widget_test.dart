4ae5541eabead6b817f38c39e7272a55b8493ae6
BrunoSabadini <bruno_sabadini@hotmail.com>
2022-06-12 16:27:29 -0300

    dont know

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:work_project/WalletPage/WalletBuild.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomeScreenStructureWidget());
+    await tester.pumpWidget(const MaterialAppAndProviderInstancesWidget());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
